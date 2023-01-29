10201ed6846114a61b298bf03702f1c746a56132
Mark Berkey <111755911+mbrky6@users.noreply.github.com>
2023-01-29 22:36:19 +0000

    Initial commit

---
+++ convert.js
@@ -0,0 +1,20 @@
+window.addEventListener("DOMContentLoaded", domLoaded);
+
+function domLoaded() {
+  document.getElementById("convertButton").addEventListener("click", convertCtoF);
+  document.getElementById("convertButton").addEventListener("click", convertFtoC);
+}
+
+function convertCtoF(degreesCelsius) {
+  degreesCelsius = document.getElementById("cInput");
+  let degreesFahrenheit = document.getElementById("fInput")
+  let fah = degreesCelsius.value * 1.8 + 32;
+  degreesFahrenheit.value = fah;
+}
+
+function convertFtoC(degreesFahrenheit) {
+  degreesFahrenheit = document.getElementById("fInput");
+  let degreesCelsius = document.getElementById("cInput")
+  let cel = (degreesFahrenheit.value - 32) / 1.8;
+  degreesCelsius.value = cel;
+}
