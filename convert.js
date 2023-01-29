60faaade8fd1cc5392f7b27596a6d67aae57cf17
Mark Berkey <111755911+mbrky6@users.noreply.github.com>
2023-01-29 22:48:36 +0000

    Added a function to make selecting between the two text boxes easier for the JavaScript file

--- convert.js
+++ convert.js
@@ -1,20 +1,31 @@
 window.addEventListener("DOMContentLoaded", domLoaded);
 
 function domLoaded() {
-  document.getElementById("convertButton").addEventListener("click", convertCtoF);
-  document.getElementById("convertButton").addEventListener("click", convertFtoC);
+  document.getElementById("convertButton").addEventListener("click", chooseConvert);
 }
+
+function chooseConvert() {
+  let textFahrenheit = document.getElementById("fInput");
+  let textCelsius = document.getElementById("cInput");
+
+  if (textFahrenheit.value === "" && textCelsius.value !== "") {
+    convertCtoF(textCelsius);
+  } // if (converting C to F)
+  else if (textFahrenheit.value !== "" && textCelsius.value === "") {
+    convertFtoC(textFahrenheit);
+  } // if (converting F to C)
+} // function chooseConvert
 
 function convertCtoF(degreesCelsius) {
-  degreesCelsius = document.getElementById("cInput");
-  let degreesFahrenheit = document.getElementById("fInput")
+  let degreesFahrenheit = document.getElementById("fInput");
   let fah = degreesCelsius.value * 1.8 + 32;
   degreesFahrenheit.value = fah;
-}
+  degreesCelsius.value = "";
+} // function convertCtoF
 
 function convertFtoC(degreesFahrenheit) {
-  degreesFahrenheit = document.getElementById("fInput");
-  let degreesCelsius = document.getElementById("cInput")
+  let degreesCelsius = document.getElementById("cInput");
   let cel = (degreesFahrenheit.value - 32) / 1.8;
   degreesCelsius.value = cel;
-}
+  degreesFahrenheit.value = "";
+} // function convertFtoC
