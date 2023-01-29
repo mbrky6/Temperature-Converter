6a1b183ea5e83e43e5c8520377ae11bad885ceef
Mark Berkey <111755911+mbrky6@users.noreply.github.com>
2023-01-29 23:40:55 +0000

    Added if/else statements to change the weather icon image depending on the converted temperature

--- convert.js
+++ convert.js
@@ -2,6 +2,7 @@ window.addEventListener("DOMContentLoaded", domLoaded);
 
 function domLoaded() {
   document.getElementById("convertButton").addEventListener("click", chooseConvert);
+  
   document.getElementById("cInput").addEventListener("input", function() { document.getElementById("fInput").value = ""; });
   document.getElementById("fInput").addEventListener("input", function() { document.getElementById("cInput").value = ""; });
 }
@@ -23,11 +24,35 @@ function convertCtoF(degreesCelsius) {
   let fah = parseFloat(degreesCelsius.value) * 1.8 + 32;
   degreesFahrenheit.value = fah;
 
+  let weather = document.getElementById("weatherImage");
+
+  if (fah > 50) {
+    weather.src = "warm.png";
+  } // if (temp > 50F)
+  else if (fah >= 32) {
+    weather.src = "cool.png";
+  } // else if (temp > 32F)
+  else {
+    weather.src = "cold.png";
+  } // else
+
 } // function convertCtoF
 
 function convertFtoC(degreesFahrenheit) {
   let degreesCelsius = document.getElementById("cInput");
   let cel = (parseFloat(degreesFahrenheit.value) - 32) / 1.8;
   degreesCelsius.value = cel;
+
+  let weather = document.getElementById("weatherImage");
+
+  if (cel > 10) {
+    weather.src = "warm.png";
+  } // if (temp > 50F)
+  else if (cel >= 0) {
+    weather.src = "cool.png";
+  } // else if (temp > 32F)
+  else {
+    weather.src = "cold.png";
+  } // else
 
 } // function convertFtoC
