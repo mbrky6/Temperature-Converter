eb37874741195ed08fbe3bd6f3d4a25948bf39f1
Mark Berkey <111755911+mbrky6@users.noreply.github.com>
2023-01-29 23:48:49 +0000

    convert.js now tests to ensure that input values are numbers, and will display a warning if they are not.

--- convert.js
+++ convert.js
@@ -2,7 +2,7 @@ window.addEventListener("DOMContentLoaded", domLoaded);
 
 function domLoaded() {
   document.getElementById("convertButton").addEventListener("click", chooseConvert);
-  
+
   document.getElementById("cInput").addEventListener("input", function() { document.getElementById("fInput").value = ""; });
   document.getElementById("fInput").addEventListener("input", function() { document.getElementById("cInput").value = ""; });
 }
@@ -20,39 +20,52 @@ function chooseConvert() {
 } // function chooseConvert
 
 function convertCtoF(degreesCelsius) {
-  let degreesFahrenheit = document.getElementById("fInput");
-  let fah = parseFloat(degreesCelsius.value) * 1.8 + 32;
-  degreesFahrenheit.value = fah;
-
-  let weather = document.getElementById("weatherImage");
-
-  if (fah > 50) {
-    weather.src = "warm.png";
-  } // if (temp > 50F)
-  else if (fah >= 32) {
-    weather.src = "cool.png";
-  } // else if (temp > 32F)
+  if (isNaN(parseFloat(degreesCelsius.value))) {
+    document.getElementById("errorMessage").innerHTML = degreesCelsius.value + " is not a number"
+  } // if (number not entered)
   else {
-    weather.src = "cold.png";
-  } // else
+    let degreesFahrenheit = document.getElementById("fInput");
+    let fah = parseFloat(degreesCelsius.value) * 1.8 + 32;
+    degreesFahrenheit.value = fah;
+
+    document.getElementById("errorMessage").innerHTML = "";
+    let weather = document.getElementById("weatherImage");
+
+    if (fah > 50) {
+      weather.src = "warm.png";
+    } // if (temp > 50F)
+    else if (fah >= 32) {
+      weather.src = "cool.png";
+    } // else if (temp > 32F)
+    else {
+      weather.src = "cold.png";
+    } // else
+  }
 
 } // function convertCtoF
 
 function convertFtoC(degreesFahrenheit) {
-  let degreesCelsius = document.getElementById("cInput");
-  let cel = (parseFloat(degreesFahrenheit.value) - 32) / 1.8;
-  degreesCelsius.value = cel;
+  if (isNaN(parseFloat(degreesFahrenheit.value))) {
+    document.getElementById("errorMessage").innerHTML = degreesFahrenheit.value + " is not a number"
+  } // if (number not entered)
 
-  let weather = document.getElementById("weatherImage");
-
-  if (cel > 10) {
-    weather.src = "warm.png";
-  } // if (temp > 50F)
-  else if (cel >= 0) {
-    weather.src = "cool.png";
-  } // else if (temp > 32F)
   else {
-    weather.src = "cold.png";
-  } // else
+    let degreesCelsius = document.getElementById("cInput");
+    let cel = (parseFloat(degreesFahrenheit.value) - 32) / 1.8;
+    degreesCelsius.value = cel;
+
+    document.getElementById("errorMessage").innerHTML = "";
+    let weather = document.getElementById("weatherImage");
+
+    if (cel > 10) {
+      weather.src = "warm.png";
+    } // if (temp > 50F)
+    else if (cel >= 0) {
+      weather.src = "cool.png";
+    } // else if (temp > 32F)
+    else {
+      weather.src = "cold.png";
+    } // else
+  }
 
 } // function convertFtoC
